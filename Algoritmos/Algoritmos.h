bb6e5ab02ee03c32cfba6cf857fa6efb6bc94615
Pedro Henrique Vargas <phrv51@gmail.com>
2026-05-22 20:24:14 -0300

    Pedro

--- Algoritmos/Algoritmos.h
+++ Algoritmos/Algoritmos.h
@@ -9,6 +9,7 @@
 void imprimeMatriz(int linha, int coluna, float **matriz, char *opcao);
 void imprimeVetor(float vetor[], int tam, char *opcao);
 void leMatrizes(FILE* arq, int tam, int qtd, float **A, float **B);
+void gaussSeidel(int tam, float **matriz, float *B, float acc);
 void gaussJacobi(int tam, float **matriz, float *B, float acc);
 void fatoracaoLU(int tam, float **A, float **L, float **U);
 void resolveLU(int tam, float **L, float **U, float *B, float X[]);
